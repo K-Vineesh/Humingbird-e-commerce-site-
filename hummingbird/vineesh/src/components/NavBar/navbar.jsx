26bf4fb42863370b44753cf92eff510e2eefb1c1
jKrithikaa <140594928+jKrithikaa@users.noreply.github.com>
2024-05-07 16:46:31 +0530

    sidebar in  navbar updated

--- hummingbird/vineesh/src/components/NavBar/navbar.jsx
+++ hummingbird/vineesh/src/components/NavBar/navbar.jsx
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css'; 
-import { FaBars, FaUser, FaSearch, FaHeart, FaShoppingCart } from 'react-icons/fa'; 
+import { FaBars, FaTimes, FaUser, FaSearch, FaHeart, FaShoppingCart } from 'react-icons/fa'; 
 import logo from './logo.jpg'; 
 
 function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <nav className="navbar" style={{ backgroundColor: 'black',display:"flex",justifyContent:'space-between' }}> 
-      <div className="navbar-left">
-        <FaBars className="navbar-icon" />
-      </div>
-      <div>
-      <a href="/" className="navbar-logo navbar-center">
-        <img src={logo} alt="Logo" className="logo-image" /> 
-      </a> </div>
-      <div className="navbar-right">
-        <ul className="navbar-links">
-          <li><a href="/login"><FaUser className="navbar-icon" /></a></li>
-          <li><a href="/search"><FaSearch className="navbar-icon" /></a></li>
-          <li><a href="/favorites"><FaHeart className="navbar-icon" /></a></li>
-          <li><a href="/cart"><FaShoppingCart className="navbar-icon" /></a></li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar" style={{ backgroundColor: 'black', display: "flex", justifyContent: 'space-between' }}> 
+        <div className="navbar-left">
+          <FaBars className="navbar-icon" onClick={toggleSidebar} />
+        </div>
+        <div>
+          <a href="/" className="navbar-logo navbar-center">
+            <img src={logo} alt="Logo" className="logo-image" /> 
+          </a>
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-links">
+            <li><a href="/login"><FaUser className="navbar-icon" /></a></li>
+            <li><a href="/search"><FaSearch className="navbar-icon" /></a></li>
+            <li><a href="/favorites"><FaHeart className="navbar-icon" /></a></li>
+            <li><a href="/cart"><FaShoppingCart className="navbar-icon" /></a></li>
+          </ul>
+        </div>
+      </nav>
+      {sidebarOpen && (
+        <div className="sidebar-overlay" onClick={toggleSidebar}>
+          <div className="sidebar" onClick={(e) => e.stopPropagation()}>
+            <FaTimes className="exit-icon" onClick={toggleSidebar} />
+            <div className="login-section">
+              <FaUser className="login-icon" />
+              <span>LOG IN</span>
+            </div>
+            <ul className="sidebar-links">
+              <li className="section-heading">NEW ARRIVALS</li>
+              <li>BEST SELLING</li>
+              <li>SHOP</li>
+              <li>TRACK ORDER</li>
+              <li>PLACE A RETURN/EXHCHANGE REQUEST </li>
+              <li>CUSTOMER SUPPORT</li>
+              <li>VISIT STORE</li>
+              <li>RELOVE</li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default Navbar;
+export default Navbar 
--- hummingbird/vineesh/src/components/announcement/announcement.js
+++ hummingbird/vineesh/src/components/announcement/announcement.js
@@ -2,7 +2,7 @@ function Announcement() {
     return (
         <div id="offer-nav">
           <p style={{margin:"0",
-          }}>FLAT 50% OFF, ON YOUR FIRST ORDER <a href="#">DOWNLOAD NOW</a> </p>
+          }}>FLAT 50% OFF, ON YOUR FIRST ORDER <a href="#" style={{color:"black"}}>DOWNLOAD NOW</a> </p>
         </div>
     );
   }
