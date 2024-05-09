cb22bfa9d038e30ae97e15e2201980f9ddce96d4
Vineesh-koppay <vkoppay@fleetstudio.com>
2024-05-09 14:14:54 +0530

    nothing

--- hummingbird/vineesh/src/App.jsx
+++ hummingbird/vineesh/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/NavBar/navbar";
 import Announcement from "./components/announcement/announcement";
-import Cards from "./components/Card/card";
+
 import ImageCarousel from "./components/display/display";
 import image1 from "./components/display/img1.jpg"
 import image2 from "./components/display/img2.jpg"
@@ -10,10 +10,10 @@ import image3 from "./components/display/img3.jpg"
 
 function App() {
   const images = [
-    
+
     image3,
-    image2,image1
-    
+    image2, image1
+
     // Add more image URLs as needed
   ];
   return (
